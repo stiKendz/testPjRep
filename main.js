53ea5b80b084e1394b8483a4f1627f1554e8c611
secondHomePC <toguedriver@gmail.com>
2025-06-17 20:40:06 +0300

    Окончание робот над стилями и версткой страниц. Дальше всё нужно оживить кодом

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
 const toMainPageButton = document.querySelector('.to_main_page');
+const toSingginButton = document.querySelector('.singin');
+const toSingupButton = document.querySelector('.singup');
+const toOffersButton = document.querySelector('.offers');
+const toAdminpageButton = document.querySelector('.adminpage');
 
 if (toMainPageButton) {
     toMainPageButton.addEventListener('click', () => {
         window.location.href = './mainpage.html';
+    });
+};
+
+if (toSingginButton) {
+    toSingginButton.addEventListener('click', () => {
+        window.location.href = './singin.html';
+    });
+};
+
+if (toSingupButton) {
+    toSingupButton.addEventListener('click', () => {
+        window.location.href = './singup.html';
+    });
+};
+
+if (toOffersButton) {
+    toOffersButton.addEventListener('click', () => {
+        window.location.href = './offers.html';
+    });
+};
+
+if (toAdminpageButton) {
+    toAdminpageButton.addEventListener('click', () => {
+        window.location.href = './admin.html';
     });
 };
